0d7fe6d9d1cb8b0b5a9a9bcc08cb474c490f37da
sahinmelikov <1rima621melikov@gmail.com>
2023-03-09 00:08:42 +0400

    c# and Js task array Metods

---
+++ Task2_Js_ap208/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Task2_Js_ap208
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////task1
+
+            /* int[] eded = { 1, 2, 3, 4,5,6,7,8,9,-1 };
+             int cem = 0;
+             for (int i = 0; i < eded.Length; i++) { 
+                 cem += eded[i];
+
+
+
+             }
+             Console.WriteLine(cem);*/
+
+            ////task 2
+            /*    string[] ad = { "salam", "necesiniz", "o" };
+                for (int i = 0; i < ad.Length; i++)
+                {
+                    if (ad[i].Length>4)
+                    {
+                        Console.WriteLine(ad[i]);
+                    }
+                }
+    */
+
+
+            ////task3
+
+           /* int[] array = { 1, 3, 0, 9, 8, 7, 6, 0, 0, 4, 5, 7, 9, 9, 8, 11, 12, 13, 14, 15 };
+
+            int sayi = 0;
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] <= 9 && array[i] >= 0)
+
+                {
+                    sayi++;
+                    
+                }
+            }
+            Console.WriteLine("arreyde" + " " + sayi + "" + " " + "reqem var");*/
+
+
+            ////task4
+
+            /*            int[] arr = { 34, 66, 3, 7, 5 };
+                        int[] arr2 = { 34, 46, 3, 5, 7 };
+                        int eded = 0;
+
+                        for (int i = 0; i < arr.Length; i++)
+                        {
+                            for (int j = 0; j < arr2.Length; j++)
+                            {
+
+                                if (arr[i] == arr2[j])
+                                {
+                                    eded = arr[i];
+                                    Console.WriteLine(eded);
+                                }
+                            }
+
+                        }*/
+
+        }
+    }
+}
